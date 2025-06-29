c170e8a3f92d9449a8c28cbfbd9bc068a0eacd9e
lillinlin <126753730+lillinlin@users.noreply.github.com>
2025-06-29 21:00:51 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,13 +11,11 @@ if ! command -v apt-get &>/dev/null; then
 fi
 
 # 2. 手动加载 BBR 模块
-echo "正在加载 BBR 模块..."
 sudo modprobe tcp_bbr
 if ! lsmod | grep -q bbr; then
     echo -e "\033[31m错误：无法加载 BBR 模块。\033[0m"
     exit 1
 fi
-echo "BBR 模块加载成功！"
 
 # 3. 检查内核是否支持 BBR
 if ! sysctl net.ipv4.tcp_available_congestion_control | grep -q bbr; then
