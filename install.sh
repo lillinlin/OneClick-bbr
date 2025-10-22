a61d0011ee61f4e23d8c9cbde09a817933597703
lillinlin <126753730+lillinlin@users.noreply.github.com>
2025-10-22 18:17:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,7 +33,6 @@ net.ipv4.tcp_sack = 1
 net.ipv4.tcp_window_scaling = 1
 net.ipv4.tcp_moderate_rcvbuf = 1
 net.ipv4.tcp_adv_win_scale = 2
-net.ipv4.tcp_frto = 2
 net.core.busy_poll = 50
 net.core.busy_read = 50
 net.ipv4.tcp_max_syn_backlog = 65535
