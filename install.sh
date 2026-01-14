799ef076f0568ea3006495fe91519678a7d82e20
lillinlin <126753730+lillinlin@users.noreply.github.com>
2026-01-14 15:38:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,11 +28,11 @@ sudo tee $CONF_FILE > /dev/null <<EOF
 net.core.default_qdisc = cake
 net.ipv4.tcp_congestion_control = bbr
 net.ipv4.tcp_slow_start_after_idle = 0
+net.ipv4.tcp_adv_win_scale = 0
 net.ipv4.tcp_timestamps = 1
 net.ipv4.tcp_sack = 1
 net.ipv4.tcp_window_scaling = 1
 net.ipv4.tcp_moderate_rcvbuf = 1
-net.ipv4.tcp_adv_win_scale = 1
 net.ipv4.tcp_mtu_probing = 1
 net.core.busy_poll = 50
 net.core.busy_read = 50
